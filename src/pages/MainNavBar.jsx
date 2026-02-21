f2a6a0e8d5c7d79ef1a94d94f3fe3275cef77350
R Sai Kiran <2500031571@kluniversity.in>
2026-02-21 10:54:43 +0530

    modified MainNavBar.jsx

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -8,7 +8,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-           KL Event Management System
+           KL Event Management System -2500031571
           </Link>
         </div>
         <ul className="nav-menu">
